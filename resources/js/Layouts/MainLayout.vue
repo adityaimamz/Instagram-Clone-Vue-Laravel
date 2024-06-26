92ddce934d26e3c67a909ecfd8706c1a225e406e
ADITYA IMAM ZUHDI <20102217@ittelkom-pwt.ac.id>
2024-06-26 14:24:19 +0700

    Make:MainLayout Section

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -16,11 +16,38 @@ let showCreatePost = ref(false);
 </script>
 
 <template>
-    <div id="MainLayout" class="bg-red-600">
-        <div>
-            MainLayout
-        </div>
+    <div id="MainLayout" class="w-full h-screen">
+        <div v-if="$page.url === '/'"
+            class="fixed z-30 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-200">
+            <div class="flex items-center justify-between h-full">
+                <Link href="/">
+                <img src="/insta-logo.png" alt="" class="w-[105px] ml-6 cursor-pointer">
+                </Link>
 
-        <slot />
+                <div class="flex items-center w-[50%]">
+                    <div class="flex items-center w-full bg-gray-100 rounded-lg">
+                        <Magnify class="pl-4" fillColor="#8e8e8e" :size="27" />
+                        <input type="text" placeholder="Search"
+                            class="bg-transparent w-full placeholder-[#8e8e8e] border-0 ring-0 focus:ring-0">
+                    </div>
+                    <HeartOutline class="pl-4 pr-3" fillColor="#000000" :size="27" />
+                </div>
+            </div>
+        </div>
+        <div v-if="$page.url !== '/'" id="TopNavUser"
+            class="md:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300">
+            <Link href="/" class="px-4">
+            <ChevronLeft :size="30" class="cursor-pointer" />
+            </Link>
+            <div class="font-extrabold text-lg">Name Here</div>
+            <AccountPlusOutline :size="30" class="cursor-pointer px-4" />
+        </div>
+        <div id="SideNav"
+            class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-300">
+            <Link href="/">
+            <img class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer" src="/insta-logo-small.png">
+            <img class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer" src="/insta-logo.png">
+            </Link>
+        </div>
     </div>
 </template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,11 +1,13 @@
 <script setup>
-    import MainLayout from '@/Layouts/MainLayout.vue';
+import MainLayout from '@/Layouts/MainLayout.vue';
 </script>
 
 <template>
 
     <Head title="Home" />
 
-    
-    <div class="bg-green-500">Home.vie</div>
+    <MainLayout>
+        <!-- <div class="bg-green-500">Home.vie</div> -->
+
+    </MainLayout>
 </template>
